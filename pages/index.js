4c7531e2973df64892fe1ab22abc1806357ae591
Łukasz Klekner <lukaszklekner@gmail.com>
2022-02-04 09:33:03 +0100

    adds Articles components and fetches data

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <div>Welcome to Next</div>
+import ArticleList from '../components/ArticleList'
+
+export default function Home({ articles }) {
+  return (
+    <div>
+      <ArticleList articles={articles} />
+    </div>
+  )
+}
+
+export const getStaticProps = async () => {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=6`
+  )
+  const articles = await response.json()
+
+  return {
+    props: {
+      articles,
+    },
+  }
 }
